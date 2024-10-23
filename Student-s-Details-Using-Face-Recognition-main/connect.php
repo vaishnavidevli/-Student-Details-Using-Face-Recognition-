1df8cc55801c06843b708a4484884b3b5626c2e0
vaishnavidevli <vaishnavidevli182@gmail.com>
2024-10-23 18:07:10 +0530

    Update connect.php

--- Student-s-Details-Using-Face-Recognition-main/connect.php
+++ Student-s-Details-Using-Face-Recognition-main/connect.php
@@ -19,7 +19,7 @@ $certificate = $_POST['certificate'];
 #database connection
  define("DB_HOST", "localhost");
  define("DB_USER", "root");
- define("DB_PASSWORD", "alapk26021973@");
+ define("DB_PASSWORD", "");
  define("DB_DATABASE", "studentdb");
  $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
  if(!$conn)
